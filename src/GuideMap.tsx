9cecd4ccf8ced62fe2556d26796157e0797d5fe1
shiba <shibatakitakuya@shibanoMacBook-Pro.local>
2024-01-18 18:27:57 +0900

    addMap

--- src/GuideMap.tsx
+++ src/GuideMap.tsx
@@ -7,7 +7,7 @@ export default function GuidMap() {
 			<Box
 				component='img'
 				width='100%'
-				src='https://firebasestorage.googleapis.com/v0/b/totonouto.appspot.com/o/img%2FGuideMap%2F20231209_%E3%81%A8%E3%81%A8%E3%81%AE%E3%81%86%E3%81%A8%20MAP.jpg?alt=media&token=9328210f-6e58-4db4-bfb6-af4f5c8b5462'
+				src='https://firebasestorage.googleapis.com/v0/b/totonouto.appspot.com/o/img%2FGuideMap%2F20231209_%E3%81%A8%E3%81%A8%E3%81%AE%E3%81%86%E3%81%A8%20MAP.jpg?alt=media&token=3418ba9f-b39f-4686-9aaa-354706789ac1'
 				alt='施設案内図'
 			/>
 		</Container>
