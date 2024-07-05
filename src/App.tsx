4b23222e3449c96574e49ada1541cb7e34a24e27
shibataki <shiba.sbtk@gmail.com>
2024-07-05 19:51:01 +0900

    add wordList

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,19 @@
 import React from 'react'
 import './App.css'
-import Header from './Header'
-import TopLogo from './TopLogo'
-import { Box, Container } from '@mui/material'
-import Introduction from './Introduction'
-import Price from './price'
-import GuidMap from './GuideMap'
-import QA from './QA'
-import Access from './Access'
-import Footer from './Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-
-const theme = createTheme({
-	typography: {
-		fontFamily: ["'Zen Old Mincho', serif"].join(','),
-	},
-})
+import TopPage from './TopPage'
+import SaunaWords from './SaunaWords'
 
 function App() {
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				<Container
-					sx={{
-						fontFamily: "'Sawarabi Mincho', serif",
-						p: 0,
-					}}
-				>
-					<Box sx={{ position: 'fixed', top: 0, zIndex: 1000, width: '100%' }}>
-						<Header />
-					</Box>
-					<Container maxWidth='sm' sx={{ mt: '40px', mb: '70px' }}>
-						<Box id='top' sx={{}}>
-							<TopLogo />
-						</Box>
-						<Box id='introduction'>
-							<Introduction />
-						</Box>
-						<Box id='price'>
-							<Price />
-						</Box>
-						<Box id='guidMap'>
-							<GuidMap />
-						</Box>
-						<Box id='QA'> </Box>
-						<QA />
-
-						<Box id='access'>
-							<Access />
-						</Box>
-					</Container>
-					<Box
-						sx={{ position: 'fixed', bottom: 0, zIndex: 1000, width: '100%' }}
-					>
-						<Footer />
-					</Box>
-				</Container>
-			</ThemeProvider>
+			<Router>
+				<Routes>
+					<Route path='/' element={<TopPage />} />
+					<Route path='saunaWords' element={<SaunaWords />} />
+				</Routes>
+			</Router>
 		</>
 	)
 }
